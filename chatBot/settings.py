6d9ad0eb45e0538833c2c3f56b15c7bac9531609
Bk <yassi_ne@live.fr>
2024-09-07 19:44:09 +0100

    Add csry

--- chatBot/settings.py
+++ chatBot/settings.py
@@ -26,22 +26,11 @@ SECRET_KEY = 'django-insecure-%ecqg&&n1wd1x*)z0!bs6veh%q=7&(1*)6&@aobjr2#+%jf15x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    ]
+ALLOWED_HOSTS = ['e-commercedjanog-production.up.railway.app', 'https://e-commercedjanog-production.up.railway.app']
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React development server
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
+CORS_ALLOWED_ORIGINS = ['https://e-commercedjanog-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://e-commercedjanog-production.up.railway.app']
 
 # Application definition
 
@@ -101,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'ZgMGGIulEHddsTAFYaXjTZdcdmyIeJvG',
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': 'junction.proxy.rlwy.net',
         'PORT': '53155',
     }
