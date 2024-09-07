6f9c95e5bff053cf6f5e26d38361098ba7554185
Bk <yassi_ne@live.fr>
2024-09-07 22:25:10 +0100

    correct some errors

--- chatBot/settings.py
+++ chatBot/settings.py
@@ -26,11 +26,6 @@ SECRET_KEY = 'django-insecure-%ecqg&&n1wd1x*)z0!bs6veh%q=7&(1*)6&@aobjr2#+%jf15x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commercedjanog-production.up.railway.app', 'https://e-commercedjanog-production.up.railway.app']
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['https://e-commercedjanog-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://e-commercedjanog-production.up.railway.app']
 
 # Application definition
 
@@ -83,18 +78,6 @@ WSGI_APPLICATION = 'chatBot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
-        'HOST': 'junction.proxy.rlwy.net',
-        'PORT': '53155',
-    }
-}
 
 
 # Password validation
